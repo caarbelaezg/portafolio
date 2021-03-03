db070c80ab461933fcb5499bdb072897e61734e4
Carlos Arbelaez <caarbelaezg@gmail.com>
2021-03-03 14:26:59 -0500

    header and navigation menu - only interfaces

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,14 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
 
 const Layout = ({ children }) => {
   return (
-    <Fragment>
+    <div className="Main">
       <Header />
       {children}
       <Footer />
-    </Fragment>
+    </div>
   );
 };
 
